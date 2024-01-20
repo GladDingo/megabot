f8788521ccac07e0226f3102129402ff5eda1ee3
GladDingo <152427178+GladDingo@users.noreply.github.com>
2024-01-19 21:26:27 -0600

    Update burger.js

--- commands/fun/burger.js
+++ commands/fun/burger.js
@@ -1,1 +1,33 @@
+const { SlashCommandBuilder, EmbedBuilder, Client, Intents, IntentsBitField } = require('discord.js');
+//const { client } = require('../../index');
+const commandUsage = new Map();
+var burgerLastCalled = 0;
+var burgerLastUser = '';
 
+function callBurger(interactionOrMessage) {
+	const now = Math.floor(Date.now() / 1000);
+	const username = interactionOrMessage.user ? interactionOrMessage.user.username : interactionOrMessage.author.username;
+	commandUsage.set('burger', {
+		timestamp: now,
+		username: username,
+	});
+	if (now >= burgerLastCalled + 21600) {
+		burgerLastCalled = commandUsage.get('burger').timestamp;
+		burgerLastUser = commandUsage.get('burger').username;
+		interactionOrMessage.reply(`:hamburger:`).catch(console.error);
+	} else {
+		interactionOrMessage.reply(`:hamburger: was already called by **${burgerLastUser}**. Please try again <t:${burgerLastCalled + 21600}:R>.`).catch(console.error);
+	}
+}
+
+module.exports.callBurger = callBurger;
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('burger')
+		.setDescription('Classic burber'),
+	async execute(interaction) {
+		callBurger(interaction);
+		console.log("Burger called via slash command.");
+	}
+};
